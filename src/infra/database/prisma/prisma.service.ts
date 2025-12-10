73611422c142486e16bdc7f6cfe1db1ca0e49b2d
Lessa Apolinario <lessaapolinario@gmail.com>
2025-12-10 09:48:15 -0300

    remove import types

--- src/infra/database/prisma/prisma.service.ts
+++ src/infra/database/prisma/prisma.service.ts
@@ -1,8 +1,4 @@
-import {
-  Injectable,
-  type OnModuleDestroy,
-  type OnModuleInit,
-} from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../../../../generated/prisma/client';
 
