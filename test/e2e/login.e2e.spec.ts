16cf0bf50ee19e59724e562dd737fe475c41fc9b
Lessa Apolinario <lessaapolinario@gmail.com>
2025-12-10 13:44:01 -0300

    Trying to fix e2e test

--- test/e2e/login.e2e.spec.ts
+++ test/e2e/login.e2e.spec.ts
@@ -1,32 +1,43 @@
 import { INestApplication } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import * as request from 'supertest';
-import type { App } from 'supertest/types';
+import { AuthRepository } from '../../src/domain/interfaces/respositories/AuthRepository';
 import { AuthModule } from '../../src/infra/auth/auth.module';
-import { AuthService } from '../../src/infra/auth/auth.service';
-import { DatabaseModule } from '../../src/infra/database/database.module';
+import { PrismaService } from '../../src/infra/database/prisma/prisma.service';
 import { InMemoryAuthRepository } from '../../src/infra/mock/auth/in_memory.auth.respository';
 
+const prismaMock = {
+  user: {
+    findUnique: jest.fn(),
+  },
+};
+
 describe('Login integration tests', () => {
-  let app: INestApplication<App>;
+  let app: INestApplication;
 
   beforeAll(async () => {
-    const testingFixture = await Test.createTestingModule({
-      imports: [DatabaseModule, AuthModule],
-      providers: [AuthService, InMemoryAuthRepository],
-    }).compile();
+    const moduleRef = await Test.createTestingModule({
+      imports: [AuthModule],
+    })
+      .overrideProvider(PrismaService)
+      .useValue(prismaMock)
+      .overrideProvider(AuthRepository)
+      .useClass(InMemoryAuthRepository)
+      .compile();
 
-    app = testingFixture.createNestApplication();
+    app = moduleRef.createNestApplication();
     await app.init();
   });
 
-  it('should not login if there is no user', async () => {
+  it('should login', async () => {
     await request.agent(app.getHttpServer()).post('/auth/register').send({
       name: 'any_name',
       last_name: 'any_last_name',
       email: 'any_email',
       password: 'any_password',
     });
+
+    prismaMock.user.findUnique.mockResolvedValue(null);
 
     const response = await request
       .agent(app.getHttpServer())
